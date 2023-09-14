c210f7e97ea5b7019c134778052da1c1f7d1eb11
yuli4ka <120272161+Yuli4ka1992@users.noreply.github.com>
2023-09-14 16:23:22 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = 'https://771c50ee-959e-4cb9-9073-9ff96464065e.serverhub.praktikum-services.ru/'
+DOC_PATH = '/docs/'
+CREATE_ORDER_PATH = '/api/v1/orders'
+GET_ORDER_INFO = '/api/v1/orders/track'

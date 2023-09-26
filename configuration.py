c619e3f0efe525b603c6f57b43d77b93da1fafbd
yuli4ka <120272161+Yuli4ka1992@users.noreply.github.com>
2023-09-26 17:24:57 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = 'https://771c50ee-959e-4cb9-9073-9ff96464065e.serverhub.praktikum-services.ru/'
+URL_SERVICE = 'https://3e09d867-bd7e-48ac-8611-e0169815039b.serverhub.praktikum-services.ru'
 DOC_PATH = '/docs/'
 CREATE_ORDER_PATH = '/api/v1/orders'
 GET_ORDER_INFO = '/api/v1/orders/track'

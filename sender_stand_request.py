7a85b8da590a0ee2b6d8745c2a21811ad182d39c
yuli4ka <120272161+Yuli4ka1992@users.noreply.github.com>
2023-09-30 18:34:29 +0300

    Add files via upload

---
+++ sender_stand_request.py
@@ -0,0 +1,9 @@
+import configuration
+import requests
+import data
+
+def post_new_order(order_body): # функция для создания заказа
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, json=data.order_body)
+
+def get_order_info(t): # функция для проверки заказа про треку
+    return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_INFO + '?t=' + str(t))

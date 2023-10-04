5755a9355057bf8083f4e12c5c9969cec417d9e0
yuli4ka <120272161+Yuli4ka1992@users.noreply.github.com>
2023-10-04 13:58:48 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -4,7 +4,6 @@ import data
 
 def post_new_order(order_body): # функция для создания заказа
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH, order_body)
-
+# Использую арумент функции, это динамический параметр, а не фиксированное значение
 def get_order_info(t): # функция для проверки заказа про треку
     return requests.get(configuration.URL_SERVICE + configuration.GET_ORDER_INFO + '?t=' + str(t))
-# Использую арумент функции, это динамический параметр, а не фиксированное значение
